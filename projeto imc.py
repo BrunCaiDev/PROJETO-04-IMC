8559e8447b00a11321c4c83306fe029b8245878c
BrunCaiDev <amebamaior@gmail.com>
2025-10-31 15:04:01 -0300

    Update projeto imc.py

--- projeto imc.py
+++ projeto imc.py
@@ -1,45 +1,26 @@
-import flet as ft
+nome = input ("Qual o seu nome?")
+peso = float  (input("Qual é o seu peso?")) 
+altura = float (input("Qual a sua altura"))
 
-def main(page: ft.Page):
-    page.title = "🎓 Galeria de Alunos"
-    page.bgcolor = "#f4f4f4"
-    page.scroll = "auto"
+imc = peso / (altura*altura)
 
-    alunos = [
-        {"nome": "Ana Clara", "curso": "Engenharia de Software", "foto": "https://i.pravatar.cc/150?img=1"},
-        {"nome": "João Pedro", "curso": "Ciência da Computação", "foto": "https://i.pravatar.cc/150?img=2"},
-        {"nome": "Mariana Silva", "curso": "Design Digital", "foto": "https://i.pravatar.cc/150?img=3"},
-        {"nome": "Lucas Almeida", "curso": "Sistemas de Informação", "foto": "https://i.pravatar.cc/150?img=4"},
-    ]
 
-    cards = []
-    for aluno in alunos:
-        card = ft.Card(
-            content=ft.Container(
-                width=220,
-                padding=10,
-                bgcolor="white",
-                border_radius=10,
-                content=ft.Column(
-                    controls=[
-                        ft.Image(src=aluno["foto"], width=200, height=200, border_radius=10),
-                        ft.Text(aluno["nome"], size=16, weight="bold", color="#1e88e5"),
-                        ft.Text(aluno["curso"], size=14, color="#555"),
-                    ],
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
-                )
-            )
-        )
-        cards.append(card)
 
-    page.add(
-        ft.Column(
-            [
-                ft.Text("🎓 Galeria de Alunos", size=26, weight="bold", color="#333"),
-                ft.Row(cards, wrap=True, alignment=ft.MainAxisAlignment.CENTER, spacing=20)
-            ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
-        )
-    )
 
-ft.app(target=main)
+
+
+
+if imc <=18.5:
+    print(f"{nome} abaixo do peso {imc}")
+elif 18.5 <= imc <= 24.9 :
+    print(f"{nome} peso normal  {imc}" )
+elif 25.0 <= imc <= 29.9 :
+    print   (f"{nome} sobrepeso {imc}") 
+elif 30.0 <= imc <= 34.9 :
+    print (f"{nome} obesidade grau I {imc}" )              
+elif 35.0 <= imc <= 39.9 :
+    print ( f"{nome} obesidade grau II {imc}") 
+elif imc >= 40 :
+       print (f"{nome}obesidade grau III {imc}")       
+else :
+     print (f"peso invalido ")
